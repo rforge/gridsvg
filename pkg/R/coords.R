d8013834197c64723384c215281430714605df51
sjp <sjp@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-08-31 13:19:16 +0000

    The main coordinate info function can now have existing info wiped. Also, the information can have information extended, and subsets overwritten.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gridsvg@126 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/coords.R
+++ pkg/R/coords.R
@@ -3,7 +3,18 @@ gridSVGCoordsGen <- function() {
   function(newcoords = NULL) {
     if (is.null(newcoords)) {
       coords
+    } else if (length(newcoords) == 1 && is.na(newcoords)) {
+      coords <<- NULL # Wipe existing info if there is only 1 NA
     } else {
+      # Keep existing information, but overwrite any existing definitions
+      # associated with any given viewport names
+      keepOldName <- ! names(coords) %in% names(newcoords)
+      if (any(keepOldName)) {
+        existingNames <- names(coords)[keepOldName]
+        for (i in 1:length(existingNames)) {
+          newcoords[[existingNames[i]]] <- coords[[existingNames[i]]]
+        }
+      }
       coords <<- newcoords
     }
   }
