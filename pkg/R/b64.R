0e730a782f5518ad1379d3d0f79a09e40f47a435
sjp <sjp@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-09-09 09:49:41 +0000

    Switch JSON library from RJSONIO to jsonlite.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gridsvg@347 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/b64.R
+++ pkg/R/b64.R
@@ -2,8 +2,7 @@ base64enc <- function(filename) {
     filesize <- file.info(filename)$size
 
     if (filesize > 0) {
-        pngdata <- .Call("b64encode", readBin(filename, "raw", n = filesize),
-                         PACKAGE = "gridSVG")
+        pngdata <- base64_enc(readBin(filename, "raw", n = filesize))
         paste0("data:image/png;base64,", pngdata, collapse = "")
     } else {
         warning(paste(sQuote(filename), "is empty"))
